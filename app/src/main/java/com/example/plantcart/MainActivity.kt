5c6084c9796318aab126fe91c8c164e6203328e8
Vanessa Lausan <73207237+VanessaLausan@users.noreply.github.com>
2020-11-08 09:48:43 +0800

    final

--- app/src/main/java/com/example/plantcart/MainActivity.kt
+++ app/src/main/java/com/example/plantcart/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.plantcart
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,17 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //login button
-        findViewById<Button>(R.id.button2).setOnClickListener{ loginButton()}
-        findViewById<Button>(R.id.button2).setOnClickListener{ backdashboard()}
+        //findViewById<Button>(R.id.button2).setOnClickListener{ loginButton()}
+        findViewById<Button>(R.id.loginbtn).setOnClickListener {
+            val usernameText = findViewById<EditText>(R.id.buttonlogin)
+            val username = usernameText.text.toString();
+
+            val passwordText = findViewById<EditText>(R.id.textPassword)
+            val password = passwordText.text.toString();
+
+            if (username.equals("Vanessa") && password.equals("gallydots")) {
+                LoginPage()
+
+            } else {
+                Toast.makeText(this, "Incorrect username or password.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
-    private fun loginButton() {
+    private fun LoginPage() {
         val intent = Intent(this, MainPage::class.java)
         startActivity(intent)
-      }
-    private fun backdashboard() {
-        val intent3 = Intent(this,MainPage::class.java)
-        startActivity(intent3)
+        Toast.makeText(this, "You are logged in", Toast.LENGTH_SHORT).show()
     }
-
 }
--- app/src/main/java/com/example/plantcart/MainPage.kt
+++ app/src/main/java/com/example/plantcart/MainPage.kt
@@ -1,13 +1,13 @@
 package com.example.plantcart
 
 import android.content.Intent
-import android.media.Image
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.ImageButton
-import androidx.core.content.ContextCompat.startActivity
+import android.widget.Toast
 
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,37 +15,60 @@ class MainPage : AppCompatActivity() {
         setContentView(R.layout.activity_main_page)
 
         //login button
-        findViewById<ImageButton>(R.id.imageButton12).setOnClickListener { backbutton () }
-        findViewById<Button>(R.id.goToShop).setOnClickListener { search () }
-        //findViewById<ImageButton>(R.id.imageButton6).setOnClickListener { settings () }
-        findViewById<ImageButton>(R.id.imageButton13).setOnClickListener { share () }
+        findViewById<ImageButton>(R.id.backbtn).setOnClickListener { backbutton() }
+        findViewById<Button>(R.id.browsebtn).setOnClickListener { search() }
+        findViewById<ImageButton>(R.id.imagebtn).setOnClickListener { Image() }
+        findViewById<Button>(R.id.messagebtn).setOnClickListener { message() }
+        findViewById<ImageButton>(R.id.settingsbtn).setOnClickListener { Settings() }
+        findViewById<ImageButton>(R.id.sharebtn).setOnClickListener { share() }
     }
 
-
+    //first 3 buttons will run successfully
     private fun backbutton() {
-        val intent2 = Intent(this, MainActivity::class.java)
-        startActivity(intent2)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
-    //private fun gotocam() {
-       // val intent2 = Intent(this, MainActivity::class.java)
-        //startActivity(intent2)
-   // }
+
     private fun search() {
         val uri = Uri.parse("https://www.google.com/search?q=succulent+plants&rlz=1C1CHBF_enPH882PH882&sxsrf=ALeKk02vwi2ApuKiVEKsONVxlFmmHrTfSg:1604676266997&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjco_72nO7sAhXUKqYKHebeAHEQ_AUoAXoECCsQAw&biw=1920&bih=880")
-        val intent3 = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(intent3)
-    }
-    //private fun settings() {
-     //   val uri = Uri.parse("content://settings/apps/1")
-     //   val intent = Intent(Intent.ACTION_VIEW, uri)
-     //   startActivity(intent)
-  //  }
-    private fun share() {
-        val uri = Uri.parse("content://settings/apps/1")
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
 
+    val REQUEST_IMAGE_GET = 1
+    fun Image() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        startActivityForResult(intent, REQUEST_IMAGE_GET)
+    }
 
-  }
+    private fun message() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+        startActivity(intent)
+    }
+
+    //next 2 buttons will fail
+    private fun Settings() {
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        intent.addCategory(Intent.ACTION_ALL_APPS)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open SETTINGS.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun share() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open SHARE.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
+
 
